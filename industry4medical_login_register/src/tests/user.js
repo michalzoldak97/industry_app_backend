1737c881ed4e26eb63ed92bbcc7cb7fa175d1b7f
michalzoldak97 <michalzoldak97@gmail.com>
2021-10-20 00:49:16 +0200

    1st unit test workin

---
+++ industry4medical_login_register/src/tests/user.js
@@ -0,0 +1,26 @@
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+const server = require("../app");
+
+chai.should();
+
+chai.use(chaiHttp);
+
+describe("Login Register API", () => {
+  describe("POST /login", () => {
+    it("It should validate user and return token", (done) => {
+      chai
+        .request(server)
+        .post("/login")
+        .set("content-type", "application/x-www-form-urlencoded")
+        .send({
+          username: `${process.env.TEST_USERNAME}`,
+          password: `${process.env.TEST_USER_PASS}`,
+        })
+        .end((err, res) => {
+          res.should.have.status(404);
+          done();
+        });
+    });
+  });
+});

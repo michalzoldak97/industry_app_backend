8823ee89e42f49463cafd4d540a5eb9ca7b404a6
michalzoldak97 <michalzoldak97@gmail.com>
2021-12-20 16:01:19 +0100

    axios handle

--- mysql_api/src/auth/userVerifier.js
+++ mysql_api/src/auth/userVerifier.js
@@ -10,9 +10,13 @@ exports.verify = catchAsync(async (req, res, next) => {
   )
     return next(new AppError("Authorization failed", 401));
   const token = req.headers.authorization.split(" ")[1];
-  const response = await axios.post(`http://login_register_api:8082/verify`, {
-    token,
-  });
+  const response = await axios
+    .post(`http://login_register_api:8082/verify`, {
+      token,
+    })
+    .catch((err) => {
+      return next(new AppError("User authentication failed", 401));
+    });
   const userId = response.data.data.userId;
   if (!userId) return next(new AppError("Invalid user data", 401));
   req.userId = userId;

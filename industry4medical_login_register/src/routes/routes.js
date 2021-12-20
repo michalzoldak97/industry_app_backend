c5bda6d44183ce99d8f6e9d333251b25c595552f
michalzoldak97 <michalzoldak97@gmail.com>
2021-12-21 00:31:24 +0100

    rate 1

--- industry4medical_login_register/src/routes/routes.js
+++ industry4medical_login_register/src/routes/routes.js
@@ -1,10 +1,21 @@
 const express = require("express");
 const router = express.Router();
 
-const { login, register } = require("../user");
+const { login, register, verify } = require("../user");
 
-router.post("/login", login);
+const rateLimit = require("express-rate-limit");
+
+const loginLimiter = rateLimit({
+  windowMs: 300000,
+  max: 3,
+  message:
+    "Too many login attempts from this IP address. Please, try again in 5 minutes",
+});
+
+router.post("/login", loginLimiter, login);
 
 router.post("/register", register);
+
+router.post("/verify", verify);
 
 module.exports = router;
